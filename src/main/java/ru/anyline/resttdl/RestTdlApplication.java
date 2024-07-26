89c1bc1b058983c9d0aa508c28c62f55c868bf58
Anyline1 <s09857303@gmail.com>
2024-07-26 13:00:07 +0300

    pom+

---
+++ src/main/java/ru/anyline/resttdl/RestTdlApplication.java
@@ -0,0 +1,13 @@
+package ru.anyline.resttdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTdlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/anyline/resttdl/RestTdlApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.anyline.resttdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTdlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
