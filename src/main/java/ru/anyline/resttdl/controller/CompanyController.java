6a5960b893603b0eee2a123f7e227c48ebab4143
Anyline1 <s09857303@gmail.com>
2024-08-22 22:42:37 +0300

    refactor+
    Signed-off-by: Anyline1 <s09857303@gmail.com>

---
+++ src/main/java/ru/anyline/resttdl/controller/CompanyController.java
@@ -0,0 +1,55 @@
+package ru.anyline.resttdl.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.anyline.resttdl.model.Company;
+import ru.anyline.resttdl.service.CompanyService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/publishers")
+public class CompanyController {
+
+    private final CompanyService companyService;
+
+    public CompanyController(CompanyService companyService) {
+        this.companyService = companyService;
+    }
+
+    @GetMapping
+    public List<Company> getAllPublishers() {
+        return companyService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> getPublisherById(@PathVariable Long id) {
+        return companyService.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Company createPublisher(@RequestBody Company company) {
+        return companyService.save(company);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Company> updatePublisher(@PathVariable Long id, @RequestBody Company company) {
+        return companyService.findById(id)
+                .map(existingCompany -> {
+                    existingCompany.setName(company.getName());
+                    return ResponseEntity.ok(companyService.save(existingCompany));
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePublisher(@PathVariable Long id) {
+        if (companyService.findById(id).isPresent()) {
+            companyService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
