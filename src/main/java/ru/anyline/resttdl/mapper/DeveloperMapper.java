d8ceb915a26b9f9687eea0cd34f75fa7aee3b7f1
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-08-23 13:53:58 +0300

    Update DeveloperMapper.java

--- src/main/java/ru/anyline/resttdl/mapper/DeveloperMapper.java
+++ src/main/java/ru/anyline/resttdl/mapper/DeveloperMapper.java
@@ -9,9 +9,7 @@ import ru.anyline.resttdl.model.Developer;
 public interface DeveloperMapper {
 
     DeveloperMapper INSTANCE = Mappers.getMapper(DeveloperMapper.class);
-
     DeveloperDTO toDeveloperDTO(Developer developer);
-
     Developer toDeveloper(DeveloperDTO developerDTO);
 
 }
