a9641a22aac0ef08703d54d1fb24f03ec9966332
Anyline1 <83272574+Anyline1@users.noreply.github.com>
2024-09-28 20:32:18 +0300

    test

--- src/test/java/ru/anyline/resttdl/ControllerTest.java
+++ src/test/java/ru/anyline/resttdl/ControllerTest.java
@@ -12,8 +12,13 @@ import ru.anyline.resttdl.DTO.ApplicationDTO;
 
 import java.util.Optional;
 
+import static jdk.internal.org.objectweb.asm.util.CheckClassAdapter.verify;
 import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.mockito.Mockito.when;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.put;
+import static org.springframework.test.web.client.ExpectedCount.times;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -31,7 +36,36 @@ public class ControllerTest {
                 .andExpect(status().isNotFound());
     }
 
+    @Test
+    public void shouldDeleteAnApplicationWhenItExists() throws Exception {
+        Long existingAppId = 1L;
+        when(applicationService.findById(existingAppId)).thenReturn(Optional.of(new ApplicationDTO(existingAppId, "Test App", "Test Developer")));
 
+        mockMvc.perform(delete("/api/applications/" + existingAppId)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        verify(applicationService, times(1)).deleteById(existingAppId);
+    }
+
+    @Test
+    public void shouldReturn405MethodNotAllowedForNonDeleteRequests() throws Exception {
+        mockMvc.perform(post("/api/applications/999999")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isMethodNotAllowed());
+
+        mockMvc.perform(put("/api/applications/999999")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isMethodNotAllowed());
+
+        mockMvc.perform(get("/api/applications/999999")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNotMethodNotAllowed());
+
+        mockMvc.perform(delete("/api/applications/999999")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+    }
 }
 
 
